a65c34763c2175668df5060f6039a723cde95ed0
victool <victordeghandi@gmail.com>
2023-10-04 19:44:48 -0300

    Estava dando erro no pacote devido estar utilizando o nome Java

--- src/main/java/Vaja/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/Vaja/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -1,4 +1,4 @@
-package java.mentoria.lojavirtual;
+package Vaja.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
