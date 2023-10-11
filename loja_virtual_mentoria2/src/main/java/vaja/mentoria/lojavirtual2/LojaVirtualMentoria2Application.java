1f9d28f23177586132234bc500e55f031ddab244
victool <victordeghandi@gmail.com>
2023-10-10 22:52:39 -0300

    Criação de classes

---
+++ loja_virtual_mentoria2/src/main/java/vaja/mentoria/lojavirtual2/LojaVirtualMentoria2Application.java
@@ -0,0 +1,13 @@
+package vaja.mentoria.lojavirtual2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentoria2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoria2Application.class, args);
+	}
+
+}
---
+++ loja_virtual_mentoria2/src/test/java/vaja/mentoria/lojavirtual2/LojaVirtualMentoria2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vaja.mentoria.lojavirtual2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoria2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
