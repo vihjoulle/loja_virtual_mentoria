1ab7e72a6544f48e9f1acba1f6e3adad6b46cc29
victool <victordeghandi@gmail.com>
2023-10-13 22:39:37 -0300

    Implementação de Classes de Model

--- loja_virtual_mentoria2/src/main/java/vaja/mentoria/lojavirtual2/model/Pessoa.java
+++ loja_virtual_mentoria2/src/main/java/vaja/mentoria/lojavirtual2/model/Pessoa.java
@@ -1,14 +1,19 @@
 package vaja.mentoria.lojavirtual2.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 
 @Entity
@@ -59,6 +64,18 @@ public abstract class Pessoa implements Serializable {
 	private String email;
 	
 	private String telefone;
+	
+	
+	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Endereco> enderecos = new ArrayList<Endereco>();
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
 
 	@Override
 	public int hashCode() {
